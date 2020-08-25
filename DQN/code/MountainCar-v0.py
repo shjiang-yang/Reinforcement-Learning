2678f255b971646a304b88a01736b0666443a8f6
shjiang-yang <49501818+shjiang-yang@users.noreply.github.com>
2020-08-25 15:29:40 +0800

    add

--- DQN/code/MountainCar-v0.py
+++ DQN/code/MountainCar-v0.py
@@ -3,16 +3,14 @@
 
 from DQN import DQN_AGENT
 import gym
-import tensorflow as tf
 
 env = gym.make('MountainCar-v0')
 
 
-agent = DQN_AGENT(action_space_n= env.action_space.n, \
-                    action_space_dim=1, \
-                    observation_space_dim=env.observation_space.shape[0], \
-                    experience_pool_size=2000)
-
+agent = DQN_AGENT(action_space_n=env.action_space.n,
+                  action_space_dim=1,
+                  observation_space_dim=env.observation_space.shape[0],
+                  experience_pool_size=2000)
 
 episode = 0
 test_per_episode = 20
@@ -29,7 +27,7 @@ while True:
         action = agent.get_action(obs)
         obs_, reward, done, info = env.step(action)
 
-        reward = max(obs_[0] + 0.2, 0.0) * 10.0 + abs(obs_[1]) * 100.0 -1.0
+        reward = max(obs_[0] + 0.2, 0.0) * 10.0 + abs(obs_[1]) * 100.0 - 1.0
 
         ready = agent.add_experience(obs, action, reward, obs_, done)
 
@@ -56,15 +54,12 @@ while True:
                 env.render()
                 observation_, reward, done, info = env.step(action)
 
-                reward = max(obs_[0] + 0.2, 0.0) * 10.0 + abs(obs_[1]) * 100.0 -1.0
-
-
+                reward = max(obs_[0] + 0.2, 0.0) * 10.0 + abs(obs_[1]) * 100.0 - 1.0
 
                 observation = observation_
                 total_reward += reward
-        
+
         agent.status_info["evaluation_scores"] = total_reward / test_episodes
-        
-    
-    
+
+
 env.close()
